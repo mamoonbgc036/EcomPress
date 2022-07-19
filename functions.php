671fcc82051e4285177c807c2bc0bfc23057d5cb
Abdullah Al Mamoon <mamoon036@gmail.com>
2022-07-19 22:13:22 +0600

    19.07.2022

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php 
+
+require get_theme_file_path( 'inc/product_search_route.php' );
+
+
 function xpent_style_and_scripts() {
 	wp_enqueue_style( 'font-awesome', get_theme_file_uri( '/css/font-awesome.min.css' ), array(), wp_get_theme()->get( 'Version' ) );
 	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.css', array(), wp_get_theme()->get( 'Version' ) );
---
+++ inc/product_search_route.php
@@ -0,0 +1,34 @@
+<?php 
+
+	add_action( 'rest_api_init', 'EcomPress_Search_Route' );
+
+	function EcomPress_Search_Route() {
+		register_rest_route( 'EcomPress/v1', 'search', array(
+			'methods' => WP_REST_SERVER::READABLE,
+			'callback' => 'ecompress_search_products',
+		) );
+	}
+
+	function ecompress_search_products() {
+		$only_cat_names = [];
+		$cats = get_categories(array(
+			'taxonomy' => 'product_cat',
+			'orderby' => 'name',
+		));
+
+		foreach ($cats as $cat) {
+			array_push( $only_cat_names, $cat->name );
+		}
+
+
+		$products = new WP_Query(array(
+			'post_type' => ['product'],
+			'orderby' => 'name'
+		));
+
+		foreach( $products->posts as $product ) {
+			array_push( $only_cat_names, $product->post_title );
+		}
+
+		return $only_cat_names;
+	}
